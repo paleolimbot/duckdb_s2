7907dff14750d52fe346ac282ee6e95832c9a8b3
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-21 10:13:39 -0500

    working versions

--- src/include/s2_versions.hpp
+++ src/include/s2_versions.hpp
@@ -1,33 +1,62 @@
-#include "duckdb/common/string_util.hpp"
-#include "duckdb/function/scalar_function.hpp"
-#include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+#include "duckdb/common/string_util.hpp"
+#include "duckdb/function/table_function.hpp"
+#include "duckdb/main/extension_util.hpp"
 
 // OpenSSL linked through vcpkg
+#include <absl/base/config.h>
 #include <openssl/opensslv.h>
 #include <s2geography.h>
 
 namespace duckdb {
 
 namespace {
-// This sort of has no arguments and returns a scalar but it's unclear how to do that
-inline void OpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, OPENSSL_VERSION_TEXT);
-        });
-}
-}
+class S2VersionsFunctionData : public TableFunctionData {
+ public:
+  S2VersionsFunctionData() : finished(false) {}
+  bool finished{false};
+};
 
-inline void RegisterVersionFunctions(DatabaseInstance &instance) {
-    // Register another scalar function
-    auto s2_openssl_version_scalar_function = ScalarFunction("s2_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, OpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, s2_openssl_version_scalar_function);
+static inline duckdb::unique_ptr<FunctionData> S2VersionsBind(
+    ClientContext& context, TableFunctionBindInput& input,
+    vector<LogicalType>& return_types, vector<string>& names) {
+  names.push_back("dependency");
+  names.push_back("version");
+  return_types.push_back(LogicalType::VARCHAR);
+  return_types.push_back(LogicalType::VARCHAR);
+  return make_uniq<S2VersionsFunctionData>();
 }
 
+static inline void S2VersionsFunction(ClientContext& context, TableFunctionInput& data_p,
+                                      DataChunk& output) {
+  auto& data = data_p.bind_data->CastNoConst<S2VersionsFunctionData>();
+  if (data.finished) {
+    return;
+  }
+
+  output.SetValue(0, 0, "openssl");
+  output.SetValue(1, 0,
+                  std::string() + std::to_string(OPENSSL_VERSION_MAJOR) + "." +
+                      std::to_string(OPENSSL_VERSION_MINOR) + "." +
+                      std::to_string(OPENSSL_VERSION_PATCH));
+  output.SetValue(0, 1, "abseil-cpp");
+  output.SetValue(1, 1,
+                  std::string() + std::to_string(ABSL_LTS_RELEASE_VERSION) + "." +
+                      std::to_string(ABSL_LTS_RELEASE_PATCH_LEVEL));
+  output.SetValue(0, 2, "s2geometry");
+  output.SetValue(1, 2,
+                  std::string() + std::to_string(S2_VERSION_MAJOR) + "." +
+                      std::to_string(S2_VERSION_MINOR) + "." +
+                      std::to_string(S2_VERSION_PATCH));
+  output.SetCardinality(3);
+  data.finished = true;
 }
 
+}  // namespace
 
+inline void RegisterVersionFunctions(DatabaseInstance& instance) {
+  TableFunction versions_func("s2_dependencies", {}, S2VersionsFunction, S2VersionsBind);
+  ExtensionUtil::RegisterFunction(instance, versions_func);
+}
+
+}  // namespace duckdb
