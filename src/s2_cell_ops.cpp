2b427d031198fc8d8019cd38d435b7426b5194d9
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-27 23:18:18 -0500

    maybe some ops

--- src/s2_cell_ops.cpp
+++ src/s2_cell_ops.cpp
@@ -13,37 +13,64 @@ namespace duckdb {
 
 namespace duckdb_s2 {
 
-using s2geography::op::point::Point;
+namespace {
 
-inline void S2CellFromPoint(DataChunk& args, ExpressionState& state, Vector& result) {
-  idx_t count = args.size();
-
-  auto& children = StructVector::GetEntries(args.data[0]);
-  auto& x = children[0];
-  auto& y = children[1];
-  auto& z = children[2];
-  x->Flatten(count);
-  y->Flatten(count);
-  z->Flatten(count);
-  double* x_ptr = reinterpret_cast<double*>(x->GetData());
-  double* y_ptr = reinterpret_cast<double*>(y->GetData());
-  double* z_ptr = reinterpret_cast<double*>(z->GetData());
-
-  s2geography::op::cell::FromPoint op;
-  for (idx_t i = 0; i < count; i++) {
-    Point pt{x_ptr[i], y_ptr[i], z_ptr[i]};
-    result.SetValue(i, static_cast<int64_t>(op.ExecuteScalar(pt)));
+struct S2CellFromPoint {
+  static void Register(DatabaseInstance& instance) {
+    auto fn = ScalarFunction("s2_cell_from_point", {Types::S2_POINT()}, Types::S2_CELL(),
+                             Execute);
+    ExtensionUtil::RegisterFunction(instance, fn);
   }
 
-  if (count == 1) {
-    result.SetVectorType(VectorType::CONSTANT_VECTOR);
+  static inline void Execute(DataChunk& args, ExpressionState& state, Vector& result) {
+    using s2geography::op::point::Point;
+    s2geography::op::cell::FromPoint op;
+
+    auto& children = StructVector::GetEntries(args.data[0]);
+
+    TernaryExecutor::Execute<double, double, double, int64_t>(
+        *children[0], *children[1], *children[2], result, args.size(),
+        [&](double x, double y, double z) {
+          Point pt{x, y, z};
+          return static_cast<int64_t>(op.ExecuteScalar(pt));
+        });
   }
+};
+
+template <typename Op, typename ArgT, typename ReturnT>
+void ExecuteUnary(DataChunk& args, ExpressionState& state, Vector& result) {
+  Op op;
+  UnaryExecutor::Execute<ArgT, ReturnT>(
+      args.data[0], result, args.size(),
+      [&](ArgT arg0) { return op.ExecuteScalar(arg0); });
 }
 
+template <typename Op>
+struct S2CellToString {
+  static void Register(DatabaseInstance& instance, const char* name) {
+    auto fn = ScalarFunction(name, {Types::S2_CELL()}, LogicalType::VARCHAR, Execute);
+    ExtensionUtil::RegisterFunction(instance, fn);
+  }
+
+  static inline void Execute(DataChunk& args, ExpressionState& state, Vector& result) {
+    Op op;
+    UnaryExecutor::Execute<int64_t, string_t>(
+        args.data[0], result, args.size(), [&](int64_t arg0) {
+          std::string_view str = op.ExecuteScalar(arg0);
+          return StringVector::AddString(
+              result, string_t{str.data(), static_cast<uint32_t>(str.size())});
+        });
+  }
+};
+
+}  // namespace
+
 void RegisterS2CellOps(DatabaseInstance& instance) {
-  auto s2_geog_point = ScalarFunction("s2_cell_from_point", {Types::S2_POINT()},
-                                      Types::S2_CELL(), S2CellFromPoint);
-  ExtensionUtil::RegisterFunction(instance, s2_geog_point);
+  using namespace s2geography::op::cell;
+
+  S2CellFromPoint::Register(instance);
+  S2CellToString<ToDebugString>::Register(instance, "s2_cell_debug_string");
+  S2CellToString<ToToken>::Register(instance, "s2_cell_token");
 }
 
 }  // namespace duckdb_s2
