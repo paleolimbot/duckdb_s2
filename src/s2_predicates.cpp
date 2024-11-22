deac46d5ba61d6da68fdb8674629a2c1c6047d81
Dewey Dunnington <dewey@fishandwhistle.net>
2024-11-22 15:01:27 -0600

    maybe make predicates.cpp more generic

--- src/s2_predicates.cpp
+++ src/s2_predicates.cpp
@@ -12,7 +12,7 @@ namespace duckdb_s2 {
 
 namespace {
 
-struct S2Predicates {
+struct S2BinaryIndexOp {
   static void Register(DatabaseInstance& instance) {
     auto mayintersect =
         ScalarFunction("s2_mayintersect", {Types::GEOGRAPHY(), Types::GEOGRAPHY()},
@@ -38,8 +38,9 @@ struct S2Predicates {
 
   static void ExecuteMayIntersectFn(DataChunk& args, ExpressionState& state,
                                     Vector& result) {
-    return ExecuteFn(args, state, result,
-                     [](UniqueGeography lhs, UniqueGeography rhs) { return true; });
+    return ExecutePredicateFn(
+        args, state, result,
+        [](UniqueGeography lhs, UniqueGeography rhs) { return true; });
   }
 
   static void ExecuteIntersectsFn(DataChunk& args, ExpressionState& state,
@@ -48,10 +49,13 @@ struct S2Predicates {
     options.set_polygon_model(S2BooleanOperation::PolygonModel::CLOSED);
     options.set_polyline_model(S2BooleanOperation::PolylineModel::CLOSED);
 
-    return ExecuteFn(
+    return ExecutePredicateFn(
         args, state, result, [&options](UniqueGeography lhs, UniqueGeography rhs) {
-          return DispatchShapeIndexFilter(std::move(lhs), std::move(rhs), options,
-                                          S2BooleanOperation::Intersects);
+          return DispatchShapeIndexFilter(
+              std::move(lhs), std::move(rhs),
+              [&options](const S2ShapeIndex& lhs_index, const S2ShapeIndex& rhs_index) {
+                return S2BooleanOperation::Intersects(lhs_index, rhs_index, options);
+              });
         });
   }
 
@@ -62,31 +66,37 @@ struct S2Predicates {
     options.set_polygon_model(S2BooleanOperation::PolygonModel::CLOSED);
     options.set_polyline_model(S2BooleanOperation::PolylineModel::CLOSED);
 
-    return ExecuteFn(
+    return ExecutePredicateFn(
         args, state, result, [&options](UniqueGeography lhs, UniqueGeography rhs) {
-          return DispatchShapeIndexFilter(std::move(lhs), std::move(rhs), options,
-                                          S2BooleanOperation::Contains);
+          return DispatchShapeIndexFilter(
+              std::move(lhs), std::move(rhs),
+              [&options](const S2ShapeIndex& lhs_index, const S2ShapeIndex& rhs_index) {
+                return S2BooleanOperation::Contains(lhs_index, rhs_index, options);
+              });
         });
   }
 
   static void ExecuteEqualsFn(DataChunk& args, ExpressionState& state, Vector& result) {
     S2BooleanOperation::Options options;
-    return ExecuteFn(
+    return ExecutePredicateFn(
         args, state, result, [&options](UniqueGeography lhs, UniqueGeography rhs) {
-          return DispatchShapeIndexFilter(std::move(lhs), std::move(rhs), options,
-                                          S2BooleanOperation::Equals);
+          return DispatchShapeIndexFilter(
+              std::move(lhs), std::move(rhs),
+              [&options](const S2ShapeIndex& lhs_index, const S2ShapeIndex& rhs_index) {
+                return S2BooleanOperation::Equals(lhs_index, rhs_index, options);
+              });
         });
   }
 
   template <typename Filter>
-  static void ExecuteFn(DataChunk& args, ExpressionState& state, Vector& result,
-                        Filter&& filter) {
-    Execute(args.data[0], args.data[1], result, args.size(), filter);
+  static void ExecutePredicateFn(DataChunk& args, ExpressionState& state, Vector& result,
+                                 Filter&& filter) {
+    ExecutePredicate(args.data[0], args.data[1], result, args.size(), filter);
   }
 
   template <typename Filter>
-  static void Execute(Vector& lhs, Vector& rhs, Vector& result, idx_t count,
-                      Filter&& filter) {
+  static void ExecutePredicate(Vector& lhs, Vector& rhs, Vector& result, idx_t count,
+                               Filter&& filter) {
     GeographyDecoder lhs_decoder;
     GeographyDecoder rhs_decoder;
     std::vector<S2CellId> intersection;
@@ -126,7 +136,6 @@ struct S2Predicates {
 
   template <typename ShapeIndexFilter>
   static bool DispatchShapeIndexFilter(UniqueGeography lhs, UniqueGeography rhs,
-                                       const S2BooleanOperation::Options options,
                                        ShapeIndexFilter&& filter) {
     if (lhs->kind() == s2geography::GeographyKind::ENCODED_SHAPE_INDEX &&
         rhs->kind() == s2geography::GeographyKind::ENCODED_SHAPE_INDEX) {
@@ -134,21 +143,21 @@ struct S2Predicates {
           reinterpret_cast<s2geography::EncodedShapeIndexGeography*>(lhs.get());
       auto rhs_index =
           reinterpret_cast<s2geography::EncodedShapeIndexGeography*>(rhs.get());
-      return filter(lhs_index->ShapeIndex(), rhs_index->ShapeIndex(), options);
+      return filter(lhs_index->ShapeIndex(), rhs_index->ShapeIndex());
     } else if (lhs->kind() == s2geography::GeographyKind::ENCODED_SHAPE_INDEX) {
       auto lhs_index =
           reinterpret_cast<s2geography::EncodedShapeIndexGeography*>(lhs.get());
       s2geography::ShapeIndexGeography rhs_index(*rhs);
-      return filter(lhs_index->ShapeIndex(), rhs_index.ShapeIndex(), options);
+      return filter(lhs_index->ShapeIndex(), rhs_index.ShapeIndex());
     } else if (rhs->kind() == s2geography::GeographyKind::ENCODED_SHAPE_INDEX) {
       s2geography::ShapeIndexGeography lhs_index(*lhs);
       auto rhs_index =
           reinterpret_cast<s2geography::EncodedShapeIndexGeography*>(rhs.get());
-      return filter(lhs_index.ShapeIndex(), rhs_index->ShapeIndex(), options);
+      return filter(lhs_index.ShapeIndex(), rhs_index->ShapeIndex());
     } else {
       s2geography::ShapeIndexGeography lhs_index(*lhs);
       s2geography::ShapeIndexGeography rhs_index(*rhs);
-      return filter(lhs_index.ShapeIndex(), rhs_index.ShapeIndex(), options);
+      return filter(lhs_index.ShapeIndex(), rhs_index.ShapeIndex());
     }
   }
 };
@@ -156,7 +165,7 @@ struct S2Predicates {
 }  // namespace
 
 void RegisterS2GeographyPredicates(DatabaseInstance& instance) {
-  S2Predicates::Register(instance);
+  S2BinaryIndexOp::Register(instance);
 }
 
 }  // namespace duckdb_s2
