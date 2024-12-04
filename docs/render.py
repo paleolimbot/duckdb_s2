4e2bccda52fc4bb165c8b0e4cb0ff59cb13030a9
Dewey Dunnington <dewey@fishandwhistle.net>
2024-12-03 23:52:52 -0600

    see if that works

--- docs/render.py
+++ docs/render.py
@@ -3,55 +3,10 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
 
-functions = [
-    {
-        "name": "s2_data_city",
-        "summary": "Return a city from the example data or error if no such city exists.",
-        "signatures": [
-            {"return_type": "GEOGRAPHY", "params": []},
-            {
-                "return_type": "GEOGRAPHY",
-                "params": [{"name": "city", "type": "VARCHAR"}],
-            },
-            {
-                "return_type": "GEOGRAPHY",
-                "params": [
-                    {"name": "city", "type": "VARCHAR"},
-                    {"name": "foofy", "type": "INTEGER"},
-                ],
-            },
-        ],
-        "category": "example-data",
-    },
-    {
-        "name": "s2_data_country",
-        "summary": "Return a country from the example data or error if no such country exists.",
-        "signatures": [
-            {"return_type": "GEOGRAPHY", "params": [{"name": "country", "type": "VARCHAR"}]},
-        ],
-        "category": "example-data",
-    },
-    {
-        "name": "s2_x",
-        "summary": "Return the longitude of a point geography or NaN if none exist.s",
-        "signatures": [
-            {
-                "return_type": "DOUBLE",
-                "params": [{"name": "geog", "type": "GEOGRAPHY"}],
-            },
-        ],
-        "category": "accessors",
-    },
-]
-
-category_names = set(fun["category"] for fun in functions)
-categories = [
-    {
-        "name": category,
-        "functions": [fun for fun in functions if fun["category"] == category],
-    }
-    for category in sorted(category_names)
-]
+def main():
+    functions = generate_functions()
+    categories = generate_categories(functions)
+    render_all(categories)
 
 
 def render_all(categories):
@@ -67,5 +22,64 @@ def render_all(categories):
         out.write(template.render(context))
 
 
+def generate_categories(functions):
+    category_names = set(fun["category"] for fun in functions)
+    return [
+        {
+            "name": category,
+            "functions": [fun for fun in functions if fun["category"] == category],
+        }
+        for category in sorted(category_names)
+    ]
+
+
+def generate_functions():
+    return [
+        {
+            "name": "s2_data_city",
+            "summary": "Return a city from the example data or error if no such city exists.",
+            "description": "An extended summary of the city",
+            "example": "SELECT s2_data_city('Toronto');",
+            "signatures": [
+                {"return_type": "GEOGRAPHY", "params": []},
+                {
+                    "return_type": "GEOGRAPHY",
+                    "params": [{"name": "city", "type": "VARCHAR"}],
+                },
+                {
+                    "return_type": "GEOGRAPHY",
+                    "params": [
+                        {"name": "city", "type": "VARCHAR"},
+                        {"name": "foofy", "type": "INTEGER"},
+                    ],
+                },
+            ],
+            "category": "example-data",
+        },
+        {
+            "name": "s2_data_country",
+            "summary": "Return a country from the example data or error if no such country exists.",
+            "signatures": [
+                {
+                    "return_type": "GEOGRAPHY",
+                    "params": [{"name": "country", "type": "VARCHAR"}],
+                },
+            ],
+            "category": "example-data",
+        },
+        {
+            "name": "s2_x",
+            "summary": "Return the longitude of a point geography or NaN if none exist.s",
+            "signatures": [
+                {
+                    "return_type": "DOUBLE",
+                    "params": [{"name": "geog", "type": "GEOGRAPHY"}],
+                },
+            ],
+            "category": "accessors",
+        },
+    ]
+
+
 if __name__ == "__main__":
-    render_all(categories)
+    main()
