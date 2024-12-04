12f2dcae7ae1ec57a077c262f2be78f911be416d
Dewey Dunnington <dewey@fishandwhistle.net>
2024-12-03 23:36:13 -0600

    more signatures

--- docs/render.py
+++ docs/render.py
@@ -7,13 +7,33 @@ functions = [
     {
         "name": "s2_data_city",
         "summary": "Return a city from the example data or error if no such city exists",
-        "signature": "s2_data_city(VARCHAR)",
-        "category": "example_data"
+        "signatures": [
+            {"return_type": "GEOGRAPHY", "params": []},
+            {
+                "return_type": "GEOGRAPHY",
+                "params": [{"name": "city", "type": "VARCHAR"}],
+            },
+            {
+                "return_type": "GEOGRAPHY",
+                "params": [
+                    {"name": "city", "type": "VARCHAR"},
+                    {"name": "foofy", "type": "INTEGER"},
+                ],
+            },
+        ],
+        "category": "example-data",
     }
 ]
 
 category_names = set(fun["category"] for fun in functions)
-categories = [{"name": category, "functions": [fun for fun in functions if fun["category"] == category]} for category in category_names]
+categories = [
+    {
+        "name": category,
+        "functions": [fun for fun in functions if fun["category"] == category],
+    }
+    for category in sorted(category_names)
+]
+
 
 def render_all(categories):
     this_dir = Path(__file__).parent
@@ -26,6 +46,7 @@ def render_all(categories):
     template = env.get_template("function-reference.md.jinja")
     with open(this_dir / "function-reference.md", "w") as out:
         out.write(template.render(context))
+
 
 if __name__ == "__main__":
     render_all(categories)
